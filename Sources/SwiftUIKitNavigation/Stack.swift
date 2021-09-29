a89b06fa431084f31ffcc03363d6add12699ba56
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2021-09-29 12:12:54 +0100

    Added generics data

--- Sources/SwiftUIKitNavigation/Stack.swift
+++ Sources/SwiftUIKitNavigation/Stack.swift
@@ -20,21 +20,33 @@ public class Stack<Content: View> {
         return vc
     }
     
-    public static func push(_ tag: Int = 1,animated:Bool = true, @ViewBuilder _ view: () -> Content) {
-        let vc = UIController(rootView: view())
+    public static func push<Content: View>(_ tag: Int = 1,animated:Bool = true,_ view: Content) {
+        let vc = UIController(rootView: view)
         vc.setTag(tag)
         NavManager.shared.navController.pushViewController(vc, animated: animated)
     }
     
-    public static func push(showAppBar: Bool = true, _ tag: Int = 1,animated: Bool = true, @ViewBuilder _ view: () -> Content) {
-        let vc = UIController(rootView: view())
+    public static func pushViews<Content: View>(_ tag: Int = 1,animated:Bool = true,_ view: [Content]) {
+        NavManager.shared.navController.viewControllers = NavManager.shared.navController.viewControllers + view.map({ view in
+            UIController(rootView: view)
+        })
+    }
+    
+    public static func pushViewsAndMakeRoot<Content: View>(_ tag: Int = 1,animated:Bool = true,_ view: [Content]) {
+        NavManager.shared.navController.viewControllers = view.map({ view in
+            UIController(rootView: view)
+        })
+    }
+    
+    public static func push<Content: View>(showAppBar: Bool = true, _ tag: Int = 1,animated: Bool = true,_ view: Content) {
+        let vc = UIController(rootView: view)
         vc.setTag(tag)
         NavManager.shared.navController.pushViewController(vc, animated: animated)
         NavManager.shared.navController.navigationBar.isHidden = !showAppBar
     }
     
-    public static func pushAndMakeRoot(showAppBar: Bool = true,animated: Bool = true, @ViewBuilder _ view: () -> Content) {
-        let vc = UIController(rootView: view())
+    public static func pushAndMakeRoot<Content: View>(showAppBar: Bool = true,animated: Bool = true,_ view: Content) {
+        let vc = UIController(rootView: view)
         vc.setTag(0)
         NavManager.shared.navController.setViewControllers([vc], animated: animated)
         NavManager.shared.navController.navigationBar.isHidden = !showAppBar

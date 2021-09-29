3158142b1ecbd64f8acbe0a7298855fc17760ad2
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2021-09-29 12:35:06 +0100

    Added rest to screens

--- Sources/SwiftUIKitNavigation/Stack.swift
+++ Sources/SwiftUIKitNavigation/Stack.swift
@@ -26,13 +26,13 @@ public class Stack<Content: View> {
         NavManager.shared.navController.pushViewController(vc, animated: animated)
     }
     
-    public static func pushViews<Content: View>(_ tag: Int = 1,animated:Bool = true,_ view: [Content]) {
+    public static func pushViews<Content: View>(_ tag: Int = 1,animated:Bool = true,_ views: [Content]) {
         NavManager.shared.navController.viewControllers = NavManager.shared.navController.viewControllers + view.map({ view in
             UIController(rootView: view)
         })
     }
     
-    public static func pushViewsAndMakeRoot<Content: View>(_ tag: Int = 1,animated:Bool = true,_ view: [Content]) {
+    public static func pushViewsAndMakeRoot<Content: View>(_ tag: Int = 1,animated:Bool = true,_ views: [Content]) {
         NavManager.shared.navController.viewControllers = view.map({ view in
             UIController(rootView: view)
         })

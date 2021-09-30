e8bf26baf8e035b1addbfe0eee1f61b0d1e4250d
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2021-09-30 12:16:56 +0100

    Fixes to Stack.push

--- Sources/SwiftUIKitNavigation/Stack.swift
+++ Sources/SwiftUIKitNavigation/Stack.swift
@@ -20,33 +20,22 @@ public class Stack<Content: View> {
         return vc
     }
     
-    public static func push<Content: View>(_ tag: Int = 1,animated:Bool = true,_ view: Content) {
-        let vc = UIController(rootView: view)
+    public static func push<Content: View>(_ tag: Int = 1,animated:Bool = true,@ViewBuilder _ view: () -> Content) {
+        let vc = UIController(rootView: view())
         vc.setTag(tag)
         NavManager.shared.navController.pushViewController(vc, animated: animated)
     }
     
-    public static func pushViews<Content: View>(_ tag: Int = 1,animated:Bool = true,_ views: [Content]) {
-        NavManager.shared.navController.viewControllers = NavManager.shared.navController.viewControllers + views.map({ view in
-            UIController(rootView: view)
-        })
-    }
     
-    public static func pushViewsAndMakeRoot<Content: View>(_ tag: Int = 1,animated:Bool = true,_ views: [Content]) {
-        NavManager.shared.navController.viewControllers = views.map({ view in
-            UIController(rootView: view)
-        })
-    }
-    
-    public static func push<Content: View>(showAppBar: Bool = true, _ tag: Int = 1,animated: Bool = true,_ view: Content) {
-        let vc = UIController(rootView: view)
+    public static func push(showAppBar: Bool = true, _ tag: Int = 1,animated: Bool = true,@ViewBuilder _ view: () -> Content) {
+        let vc = UIController(rootView: view())
         vc.setTag(tag)
         NavManager.shared.navController.pushViewController(vc, animated: animated)
         NavManager.shared.navController.navigationBar.isHidden = !showAppBar
     }
     
-    public static func pushAndMakeRoot<Content: View>(showAppBar: Bool = true,animated: Bool = true,_ view: Content) {
-        let vc = UIController(rootView: view)
+    public static func pushAndMakeRoot<Content: View>(showAppBar: Bool = true,animated: Bool = true,@ViewBuilder _ view: () -> Content) {
+        let vc = UIController(rootView: view())
         vc.setTag(0)
         NavManager.shared.navController.setViewControllers([vc], animated: animated)
         NavManager.shared.navController.navigationBar.isHidden = !showAppBar
